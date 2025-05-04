7fbe85a23192db5a81c3d518026d2622b5c55baa
mehtasanchit <mehtasanchit3@gmail.com>
2025-05-04 14:32:17 +0530

    some changes

--- src/main/java/auth/RegisterServlet.java
+++ src/main/java/auth/RegisterServlet.java
@@ -4,11 +4,9 @@ import db.MongoDBUtil;
 import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 
-@WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
